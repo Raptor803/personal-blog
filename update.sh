e0fa57b6af0209b741b51835f726a514c6213acb
Gionatan Buono <gionatan9703@gmail.com>
2024-04-11 20:08:47 +0200

    added update script for raptor803.github.com

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -r "/media/Bolt/MEGA/Software Projects/raptor803.github.io/*"
+cp -r "/media/Bolt/MEGA/Software Projects/personal-blog/blog/public" "/media/Bolt/MEGA/Software Projects/raptor803.github.io"
